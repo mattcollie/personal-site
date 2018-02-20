4f6b1685db8c3548e5c1cafbab98d90fccc2de77
matt <mattcollie@live.com>
2018-02-20 22:24:26 +1300

    adding contact controller to save contact form submissions

---
+++ web/web/Controllers/ContactController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace web.Controllers
+{
+    public class ContactController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index(string senderName, string subjectText, string messageText)
+        {
+            // save the contact request
+            return Ok();
+        }
+    }
+}
